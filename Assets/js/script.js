6d126751508bcb96bb4144cec6c2cbf91e587859
kitanablade <kiteagle@gmail.com>
2022-08-31 19:24:58 -0700

    pseudocode, basic html

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+const openWXapiKey = a8d8c2258511aba345cf88e92d799e37;
+/*
+Search or saved buttons onclick ->
+
+getWXdata(cityName){
+    if city = null, polite error message
+    if city ! null:
+    get data from API
+    populate info on main display
+    populate info on cards (loop)}
+    add city to set, local storage
+
+    loop through set and create buttons
+
+
+*/
+
