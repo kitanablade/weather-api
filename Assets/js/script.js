b8a37c6c825da177dc72df992dbe9f2600ec27c0
kitanablade <kiteagle@gmail.com>
2022-09-01 22:24:25 -0700

    working local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-const openWXapiKey = a8d8c2258511aba345cf88e92d799e37;
+const openWXapiKey = "a8d8c2258511aba345cf88e92d799e37";
 /*
 Search or saved buttons onclick ->
 
@@ -11,7 +11,39 @@ getWXdata(cityName){
     add city to set, local storage
 
     loop through set and create buttons
-
-
 */
 
+
+function getSrchInput(event) {
+    event.preventDefault();
+    var citiesArray = JSON.parse(localStorage.getItem("cities"));
+    city = document.querySelector("#cityTextInput").value;
+    if (citiesArray === null) {
+        localStorage.setItem("cities", JSON.stringify([city]));
+    }else{
+        citiesArray.push(city);
+        localStorage.setItem("cities", JSON.stringify(citiesArray));
+        console.log("Local Storage: ", citiesArray);
+    }
+    // cities.push(storedCities);
+    // localStorage.setItem("city" + cities);
+    // citySearch(nameCapitalized);
+    // const openWeatherURL =
+    //   "https://api.openweathermap.org/data/2.5/weather?q=" +
+    //   city +
+    //   "&appid=" +
+    //   openWXapiKey +
+    //   "&units=imperial";
+}
+
+function getWXdata(city) {
+  if (cityName === null) {
+    return;
+  } else {
+    const wxData = fetch(openWeatherURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+}
